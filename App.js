0b39ac4c3fc9d3cdf766f6efea54e8dceffb324a
zining0109 <tanzining0109@gmail.com>
2024-11-26 11:07:27 +0800

    Update App.js

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; //browserRouter is the main container for all the routes in a React application
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom'; //browserRouter is the main container for all the routes in a React application
 import IcRecognition from './components/icRecognition';
 import FaceScan from './components/FaceScan';
 
@@ -11,6 +11,9 @@ function App() {
             <div className="App">
                 <h1>Store Rental Recognition System</h1>
                 <Routes>
+                    {/* Redirect the root "/" path to "/icRecognition" */}
+                    <Route path="/" element={<Navigate to="/icRecognition" replace />} />
+        
                     {/* Route for IC Recognition page */}
                     <Route path="/icRecognition" element={<IcRecognition setTenant={setTenant} />} /> {/*specify a path prop that defines the URL, and the element prop that defines which React component should be rendered when the path is matched*/}
                     
